cb1255e8f770bc52147f7540cdd14c0c0660bd14
KittySkyHawk <102040217+KittySkyHawk@users.noreply.github.com>
2022-09-21 11:15:26 -0400

    Update Skywatch.py

--- Skywatch.py
+++ Skywatch.py
@@ -2386,9 +2386,9 @@ def optimize_area_group(gdfclean,quote_type,minarea,filepath=''):
     if quote_type == "Tasking High Res" or quote_type == "Tasking Very High Res":
         radius=200  #radius to buffer in iteration (in m)
         print(f'Tasking')
-        minarea=75
+        minarea=60
         radius=300  #radius to buffer in iteration (in m)
-        buffer_interval=25
+        buffer_interval=10
         start_interval=buffer_interval
         print(f'Tasking buff int is {buffer_interval}')
     
