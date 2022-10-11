137ae9d09b995e4a995e958fcba68cb3ff506615
KittySkyHawk <102040217+KittySkyHawk@users.noreply.github.com>
2022-10-11 10:55:39 -0400

    Update Skywatch.py

--- Skywatch.py
+++ Skywatch.py
@@ -2741,3 +2741,305 @@ def archive_coverage(gdf,start_date,end_date,api_key,data_type,coverage_resoluti
     else:
         no_results='the search did not work'
         return no_results
+    
+def divider(factorlist,divideby,maxwidth,maxheight):        
+    
+    ### IF REALLY TALL just divide height up
+    
+    if maxwidth/maxheight <= 0.125:
+        print('<0.125')
+        height = maxheight/divideby
+        width = maxwidth
+
+    ### IF pretty tall increase the factorlist
+    
+    elif maxwidth/maxheight <0.33:
+        print('<0.33')
+        while len(factorlist)==2:
+            factorlist=[]
+            for i in range(1, divideby+2):
+                #print(f'i is {i}')
+                divide=divideby+1
+                if divide%i==0:
+                    print(f'divideby+1 is {divideby+1}')
+                    print((divideby)%i)
+                    factorlist.append(i)
+            divideby=divide
+            
+        else:
+            factorsplit=[]
+            factorsplit.append(factorlist[1])
+            factorsplit.append(factorlist[-2])
+
+        height=maxheight/factorsplit[1]
+        width=maxwidth/factorsplit[0]
+
+    elif maxwidth/maxheight <=1:
+        print('<1')
+        while len(factorlist)<=4:
+            factorlist=[]
+            for i in range(1, divideby+2):
+                #print(f'i is {i}')
+                divide=divideby+1
+                if divide%i==0:
+                    print(f'divideby+1 is {divideby+1}')
+                    print((divideby)%i)
+                    factorlist.append(i)
+            divideby=divide
+        if len(factorlist)==6:
+            factorsplit=[]
+            factorsplit.append(factorlist[2])
+            factorsplit.append(factorlist[3])
+
+        elif len(factorlist)==8:
+            factorsplit=[]
+            factorsplit.append(factorlist[3])
+            factorsplit.append(factorlist[4])
+        elif len(factorlist)==10:
+            factorsplit=[]
+            factorsplit.append(factorlist[4])
+            factorsplit.append(factorlist[5])
+            
+        else:
+            factorsplit=[]
+            factorsplit.append(factorlist[1])
+            factorsplit.append(factorlist[-2])
+
+        height=maxheight/factorsplit[1]
+        width=maxwidth/factorsplit[0]
+        
+
+    elif maxwidth/maxheight <2:
+        print('<2')
+        while len(factorlist)<=3:
+            factorlist=[]
+            for i in range(1, divideby+2):
+                #print(f'i is {i}')
+                divide=divideby+1
+                if divide%i==0:
+                    print(f'divideby+1 is {divideby+1}')
+                    print((divideby)%i)
+                    factorlist.append(i)
+            divideby=divide
+        if len(factorlist)==6:
+            factorsplit=[]
+            factorsplit.append(factorlist[3])
+            factorsplit.append(factorlist[2])
+
+        elif len(factorlist)==8:
+            factorsplit=[]
+            factorsplit.append(factorlist[3])
+            factorsplit.append(factorlist[4])
+        elif len(factorlist)==10:
+            factorsplit=[]
+            factorsplit.append(factorlist[4])
+            factorsplit.append(factorlist[5])
+        elif len(factorlist)==12:
+            factorsplit=[]
+            factorsplit.append(factorlist[5])
+            factorsplit.append(factorlist[6])
+            
+            
+        else:
+            factorsplit=[]
+            factorsplit.append(factorlist[1])
+            factorsplit.append(factorlist[-2])
+
+
+        height=maxheight/factorsplit[0]
+        width=maxwidth/factorsplit[1]
+
+
+    elif maxwidth/maxheight <6:
+        print('<6')
+        print(factorlist)
+        while len(factorlist)==2:
+            factorlist=[]
+            for i in range(1, divideby+2):
+                #print(f'i is {i}')
+                divide=divideby+1
+                if divide%i==0:
+                    print(f'divideby+1 is {divideby+1}')
+                    print((divideby)%i)
+                    factorlist.append(i)
+            divideby=divide
+        if len(factorlist)==4:
+            print(f'factorlist4 {factorlist}')
+            factorsplit=[]
+            factorsplit.append(factorlist[2])
+            factorsplit.append(factorlist[1])
+            print(factorsplit)
+
+        elif len(factorlist)==6:
+            print('factorlist6')
+            factorsplit=[]
+            factorsplit.append(factorlist[3])
+            factorsplit.append(factorlist[2])
+            
+        else:
+            factorsplit=[]
+            factorsplit.append(factorlist[1])
+            factorsplit.append(factorlist[-1])
+
+        print(factorsplit) # 4,3
+        height=maxheight/factorsplit[1]
+        print(height)
+        width=maxwidth/factorsplit[0]
+        print(width)
+
+    #elif maxwidth/maxheight <= 12:
+    else:
+        print(divideby)
+        height = maxheight
+        width = maxwidth/(divideby)
+        
+    return [width,height]
+
+def EAPGrid_new(gdf):
+    
+    #Check the area of the input and if it's >600 split it.
+    gdfarea=sw.aoi_areakm(gdf,'area')
+    gdfarea_area=gdfarea['area'][0]
+        
+    
+    
+    #Iterate through gdf 
+    
+    outgdf=gpd.GeoDataFrame()
+    for cur_row in gdf:
+        cur_row_gdf=gdf.iloc[[0]]
+        #Create Centroid for projection - this assumes that the index of the item is 0 and there is only 1 AOI polygon is being added OR it is being passed a new iloc gdf.
+        if cur_row_gdf.geom_type[0] in ['Point','Polygon', 'MultiPolygon']:
+            pol2=gdf.centroid[0]
+            print(gdf.centroid[0])
+            pass
+        else:
+            print("not valid geometry")
+
+        #print(pol2)
+        wgs84_globe = pyproj.Proj(proj='longlat', ellps='WGS84')
+
+        aeqd = pyproj.Proj(proj='aeqd', ellps='WGS84', datum='WGS84', lat_0=pol2.y, lon_0=pol2.x)
+
+        projpol = sh_transform(partial(pyproj.transform, wgs84_globe, aeqd), cur_row_gdf['geometry'][0]) 
+        print(projpol)
+        temp=gpd.GeoSeries(projpol)
+        boundary=temp.bounds
+
+        # get length of bounding box edges
+        edge_width = (Point(boundary['minx'][0],boundary['miny'][0])).distance(Point(boundary['maxx'][0],boundary['miny'][0]))
+        edge_height =(Point(boundary['minx'][0],boundary['miny'][0])).distance(Point(boundary['minx'][0],boundary['maxy'][0]))
+
+        print(f'max width is = {edge_width}, max height is = {edge_height}')
+
+        poly_geometry = [
+                    (boundary['minx'][0], boundary['miny'][0]),
+                    (boundary['minx'][0], boundary['maxy'][0]),
+                    (boundary['maxx'][0], boundary['maxy'][0]),
+                    (boundary['maxx'][0], boundary['miny'][0]),
+                    (boundary['minx'][0], boundary['miny'][0]),
+                    ]
+
+
+        
+        newppp=(sh_transform(partial(pyproj.transform, aeqd, wgs84_globe), Polygon(poly_geometry)))
+        print(newppp)
+        #poly_geometry=Polygon(newppp)
+        #print(poly_geometry)
+        gs=gpd.GeoSeries(newppp)
+        polygdf=gpd.GeoDataFrame(geometry=gs)
+        #print(polygdf.crs)
+        polygdf=sw.aoi_areakm(deepcopy(polygdf),'area')
+        gdf_area=sw.aoi_areakm(deepcopy(gdf),'area')
+        area=gdf_area['area'][0]
+        bbarea=polygdf['area'][0]
+
+
+
+        print(f'bounding area = {bbarea}')
+
+        divideby=int(math.ceil(bbarea/50)) 
+
+        print(f'divideby is {divideby}')
+
+        boundsarea=bbarea/divideby
+        area=boundsarea*1000000
+        print(area)
+
+        minwidth = 1000
+        maxwidth=edge_width
+        maxheight=edge_height
+
+        #print(f'area is {area}')
+        print(f'maxwidth is {maxwidth}')
+
+
+        val_range = range(0,100)
+
+
+        
+
+        factorlist=[]
+        if divideby >3:
+            #Add if maxwidth/maxheight factor for option of single row
+            print('divide by >3')
+            for i in range(1, divideby+1):
+                if divideby % i == 0:
+                    factorlist.append(i)
+            print(factorlist)
+            division=divider(factorlist,divideby,maxwidth,maxheight)        
+            width=division[0]
+            height=division[1]
+
+
+
+            # Python Program to find the factors of a number
+        elif divideby<4 or maxwidth/maxheight <=0.5 or maxwidth/maxheight >=5:
+            print('divideby <4 or high ratio')
+            if maxwidth/maxheight <=1:
+                height = maxheight/divideby
+                width = maxwidth
+            elif maxwidth/maxheight >1:
+                height=maxheight
+                width=maxwidth/divideby
+                print(f'width is {width}')
+#Edited to remove branching logic
+        #else: #divideby <= 12:
+            #print('divideby <= 12')
+            else:
+
+                division=divider(factorlist,divideby,maxwidth,maxheight)        
+                width=division[0]
+                height=division[1]
+      
+
+
+        cols = list(np.arange(boundary['minx'][0], boundary['maxx'][0]+int(math.ceil(width)), int(math.ceil(width))))
+        print(cols)
+        rows = list(np.arange(boundary['miny'][0], boundary['maxy'][0]+int(math.ceil(height)), int(math.ceil(height))))
+        print(rows)
+        polyproj=[]
+        for x in cols[:-1]:
+            #print(x)
+            for y in rows[:-1]:
+                #print(y)
+                newpoly=(Polygon([(x,y), (x+width, y), (x+width, y+height), (x, y+height)]))
+                newpolypro=(sh_transform(partial(pyproj.transform, aeqd, wgs84_globe), newpoly))
+                polyproj.append(newpolypro)  
+
+        gdfname=gpd.GeoDataFrame(geometry=polyproj)
+        gdfname=sw.aoi_areakm(deepcopy(gdfname),'area')
+        #Totalarea = gdfname['area'].sum()
+        #print(Totalarea)
+        print(len(gdfname))
+        #print(type(gdfname))  
+        #if len(gdfname)==divideby:
+            #if Totalarea < area:
+        polygons=outgdf.append(gdfname)
+        print(type(polygons))
+    
+        
+
+
+    return polygons
+    #return polyGS
